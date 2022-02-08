c931976a41e3d927d00a6f56bf42d53f9efc59e4
shimasawa <honnomushi@rakuten.jp>
2022-02-09 00:07:09 +0900

    未検証かつ未完成部分あり

--- SimpleRPG.py
+++ SimpleRPG.py
@@ -1,3 +1,4 @@
+from hashlib import new
 import json
 import os
 import sys
@@ -20,22 +21,49 @@ class Json:
             json.dump(Dict,f,indent=2)
 
 class Map_Control:
-    def __init__(self):
-        pass
+    def __init__(self,MAP_SIZE):
+        self.MAP_SIZE = MAP_SIZE
 
-    def generate(self,p_xy):#プレイヤーの座標
-        map_data = [["+" for i in range(20)] for i in range(10)]
-        map_data[p_xy[1]][p_xy[0]] = "p"
+    def generate(self,MAP_SIZE,p_xy):#プレイヤーの座標
+        map_data = [["+" for i in range(MAP_SIZE[0])] for i in range(MAP_SIZE)[1]]
+        map_data = self.add_entity(map_data,"p",p_xy)
         
         return map_data
     
-    def printm(self,map_data):
+    def save(self,map_data,state_data):
+        j =  Json("setting.json")
+        Dict = j.load()
+        Dict["map_data"] = map_data
+        Dict["state_data"] = state_data
+        Json("setting.json").dump(Dict)
+
+    def draw(self,map_data):
         
         for i in map_data:
             print("".join(i))
+
+    def add_entity(self,map_data,entity,xy):
+        map_data[xy[1]][xy[0]] = entity
+
+        return map_data
     
-    def xy_generator(self,x,y): #x,yの上限
-        xy = [random.randrange(x),random.randrange(y)]
+    def load(self,state_data,map_data=None):
+
+        if state_data == "new":
+            state_data = [
+                self.xy_generator(self.MAP_SIZE),
+                [self.xy_generator(self.MAP_SIZE) for i in range(3)],
+                [self.xy_generator(self.MAP_SIZE) for i in range(3)]
+            ]
+        
+        map_data = self.generate(self.MAP_SIZE,state_data[0])
+        
+        for xy in state_data[1]:
+            self.add_entity(map_data,"e",xy)
+            
+
+    def xy_generator(self,map_size): #x,yの上限
+        xy = [random.randrange(map_size[0]),random.randrange(map_size[1])]
 
         return xy
     
@@ -65,9 +93,24 @@ class Game:
             self.p_name = input("君の名前を教えて!\n→")
             self.j.dump({
                     "name":self.p_name,
-                    "map_data":["new"]
+                    "state_data":["new"]
             })
             print("以下の名前でセーブデータを作成しました\n→",self.p_name)
+
+    def preparation(self):
+        MAP_SIZE = [20,10]
+        Map = Map_Control(MAP_SIZE)
+        j = Json("setting.json").load()
+        self.state_data = j["state_data"][0]
+
+        self.map_data = Map.load(self.state_data)
+        entity_state = j["entity_state"]
+        print("以下のマップをロードしました")
+        Map.draw(self.map_data)
+
+    def main(self):
+        self.preparation()
+        
 
     def run(self):
         
@@ -75,7 +118,7 @@ class Game:
             selector = input("\nSIMPLE RPG\n\na:START\nb:DATACLEAR\nc:END\n→")
             
             if selector == "a":
-                Main()
+                self.main()
 
             if selector == "b":
                 input_name = input("二重確認:現在の名前を入力してください\n→")
@@ -84,6 +127,7 @@ class Game:
                     Json("setting.json").dump({})
                     input("データを削除しました。再起動してください。")
                     sys.exit()
+                
                 input("確認に失敗しました\nタイトルに戻ります。")
             
             if selector == "c":
@@ -91,21 +135,6 @@ class Game:
                 sys.exit()
 
             continue
-
-class Main:
-        def __init__(self):
-            Map = Map_Control()
-            j = Json("setting.json").load()
-            self.map_data = j["map_data"][0]
-
-            if self.map_data == "new":
-                p_xy = Map.xy_generator(20,10)
-                input(p_xy)
-                self.map_data = Map.generate(p_xy)
-            
-            print(Map.xy_searcher("p",self.map_data),p_xy)
-            Map.printm(self.map_data)
-            
 
 if __name__ == "__main__":
     Game = Game()
